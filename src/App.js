bc4979d1b04cb6cd87dc90e8026b509aefbebe7f
Yahya Khodr <yhy97kh18@gmail.com>
2022-02-26 22:07:18 +0200

    Tested the first login api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+
+ const regUrl= "http://127.0.0.1:8000/api/auth/login";
 
 function App() {
+  const [loginEmail,setLoginEmail] = useState('');
+  const [loginPassword,setLoginPassword] = useState('');
+  const submitLogin = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    axios.post(regUrl,{
+      email:loginEmail,
+      password:loginPassword
+    }).then((response)=> {
+      console.log(response.data);
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <input type="text" name="email" id="email" placeholder="email" onChange={(e)=>{
+        setLoginEmail(e.target.value)
+      }}/>
+      <input type="text" name="password" id="password" placeholder="password" onChange={(e)=>{
+        setLoginPassword(e.target.value)
+      }}/>
+      <input type="button" value="register" onClick={submitLogin} />
     </div>
   );
 }
