14e0c7899d814fa2d30f4fe5d80a1af6f65f4523
Yahya Khodr <yhy97kh18@gmail.com>
2022-02-28 16:08:27 +0200

    Added about us page

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -99,7 +99,7 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Yahya Khodr" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Avatar" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
